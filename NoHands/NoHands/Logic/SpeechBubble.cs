7a22e798ef496dc300f1958a6490d63fe9a5d2ba
gn.fur <patych7@gmail.com>
2018-08-08 19:36:10 +0300

    Added nice speech bubble.

--- NoHands/NoHands/Logic/SpeechBubble.cs
+++ NoHands/NoHands/Logic/SpeechBubble.cs
@@ -18,7 +18,8 @@ namespace NoHands.Logic
 		public readonly Actor Owner;
 		public const int Border = 16;
 
-		
+		float _textRubberBand = 8f;
+
 		public Vector2 Offset = new Vector2(0, -120);
 
 		AutoAlarm _typeAlarm = new AutoAlarm(0.05);
@@ -27,11 +28,35 @@ namespace NoHands.Logic
 
 		IFont _font = Resources.Fonts.CartonSix;
 
+		Vector2 _textSize, _targetTextSize;
+		
+		int _cornerVecticesCount = 4;
+		int _sideVecticesCount = 8;
+
+		Vector2[] _wiggleys;
+		double[] _wiggleysPhase;
+		float _wiggleysSpd = 0.25f * 20;
+		float _wiggleysR = 1;
+		int _wiggleyId = 0;
+
 		public SpeechBubble(Actor owner, string text)
 		{
 			Owner = owner;
 			Text = text;
+
+			var r = new RandomExt();
+
+			_wiggleys = new Vector2[(_cornerVecticesCount + _sideVecticesCount) * 4];
+			_wiggleysPhase = new double[_wiggleys.Length];
+
+			for(var i = 0; i < _wiggleysPhase.Length; i += 1)
+			{
+				_wiggleysPhase[i] = r.NextDouble(Math.PI * 2);
+			}
+
 		}
+
+
 
 		public override void Update()
 		{
@@ -61,15 +86,40 @@ namespace NoHands.Logic
 					_typeAlarm.Active = false;
 					TextPtr = Text.Length - 1;
 					_delayAlarm.Set(1 + Text.Length * 0.1);
-				}
+				}		
 			}
+
+			var str = Text.Substring(0, TextPtr + 1);
+			_targetTextSize = _font.MeasureString(str);
+
+			_textSize.X += (_targetTextSize.X - _textSize.X) / _textRubberBand;
+			_textSize.Y += (_targetTextSize.Y - _textSize.Y) / _textRubberBand;
+
 
 			if (_delayAlarm.Update())
 			{
 				Objects.Destroy(this);
 			}
+
+			var phaseAdd = GameCntrl.Time(_wiggleysSpd);
+
+			for(var i = 0; i < _wiggleys.Length; i += 1)
+			{
+				_wiggleysPhase[i] += phaseAdd;
+				if (_wiggleysPhase[i] > Math.PI * 2)
+				{
+					_wiggleysPhase[i] -= Math.PI * 2;
+				}
+
+				_wiggleys[i] = new Vector2(
+					(float)Math.Cos(_wiggleysPhase[i]), 
+					(float)Math.Sin(_wiggleysPhase[i])
+				) * _wiggleysR;
+			}
 			
 		}
+
+
 
 		public override void DrawEnd()
 		{
@@ -82,12 +132,63 @@ namespace NoHands.Logic
 			DrawCntrl.HorAlign = TextAlign.Center;
 			DrawCntrl.VerAlign = TextAlign.Center;
 
-			var textSize = _font.MeasureString(str) + Vector2.One * Border;
+			DrawSpeechBubble(orig, _textSize);
 
-			DrawCntrl.DrawRectangle(orig - textSize / 2, orig + textSize / 2, false);
-			
 			DrawCntrl.CurrentColor = new Color(Color.Black, 0.7f);
 			DrawCntrl.DrawText(str, orig);
 		}
+
+
+
+		void DrawSpeechBubble(Vector2 pos, Vector2 size)
+		{
+			DrawCntrl.CurrentColor = new Color(Color.White, 1.5f);
+
+			_wiggleyId = 0;
+
+			var flippedSize = new Vector2(size.X, -size.Y);
+
+			DrawCntrl.PrimitiveBegin();
+		
+			DrawArc(pos - size / 2, 90);
+			DrawLine(pos - size / 2, Vector2.UnitX, size.X);
+			
+			DrawArc(pos + flippedSize / 2, 0);
+			DrawLine(pos + flippedSize / 2, Vector2.UnitY, size.Y);
+
+			DrawArc(pos + size / 2, 270);
+			DrawLine(pos + size / 2, -Vector2.UnitX, size.X);
+
+			DrawArc(pos - flippedSize / 2, 180);
+			DrawLine(pos - flippedSize / 2, -Vector2.UnitY, size.Y);
+			
+			//DrawCntrl.PrimitiveSetLineStripIndices(true);
+			DrawCntrl.PrimitiveSetTriangleFanIndices();
+			DrawCntrl.PrimitiveEnd();
+		}
+
+		void DrawArc(Vector2 pos, float ang)
+		{
+			var stepAng = 90f / _cornerVecticesCount;
+			for(var i = _cornerVecticesCount; i >= 0; i -= 1)
+			{
+				var dir = MathHelper.ToRadians(ang + i * stepAng);
+				DrawCntrl.PrimitiveAddVertex(pos + _wiggleys[i] + new Vector2((float)Math.Cos(dir), -(float)Math.Sin(dir)) * Border);
+				_wiggleyId += 1;
+			}
+		}
+
+		void DrawLine(Vector2 pos, Vector2 dir, float length)
+		{
+			var step = length / _sideVecticesCount;
+			var borderOffset = new Vector2(dir.Y, -dir.X) * Border;
+			for(var i = 0; i <= _sideVecticesCount; i += 1)
+			{
+				DrawCntrl.PrimitiveAddVertex(pos + _wiggleys[i] + dir * step * i + borderOffset);
+				_wiggleyId += 1;
+			}
+		}
+
+
 	}
 }
